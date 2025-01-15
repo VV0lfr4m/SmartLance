70a96b91f38290a4ab0d82485d169a3847456134
VV0lfr4am <vladlen.tsykin@gmail.com>
2025-01-15 13:51:42 +0200

    Task Manager complete

--- src/blockchain/contracts/TaskManager.sol
+++ src/blockchain/contracts/TaskManager.sol
@@ -13,6 +13,7 @@ contract TaskManager {
         uint budget;
         uint endDate;
         bool isCompleted;
+        bool isConfirmed;
     }
 
     event TaskCreated(
@@ -24,14 +25,20 @@ contract TaskManager {
     );
     event TaskAssigned(uint indexed taskId, address indexed executor);
     event TaskCompleted(uint indexed taskId);
+    event TaskConfirmed(uint indexed taskId, address indexed owner);
 
     mapping(uint => Task) private tasks;
     uint private taskCount;
+
+    modifier existingTask(uint _taskId) {
+        require(taskExists(_taskId), "TaskManager.acceptTask: Task does not exist");
+    }
 
     function createTask(string calldata _description, uint _budget, uint _endDate) external {
         require(bytes(_description).length > 0, "TaskManager.createTask: description should not be empty!");
         require(_budget > 0, "TaskManager.createTask: budget should be greater then 0!");
         require(_endDate > block.timestamp, "TaskManager.createTask: endDate must be in the future!");
+        require(msg.value >= _budget, "TaskManager.createTask: Insufficient funds sent!");
 
         taskCount++;
 
@@ -41,13 +48,14 @@ contract TaskManager {
         budget : _budget,
         endDate : _endDate,
         isCompleted : false,
+        isConfirmed : false,
         executor : address(0)
         });
 
         emit TaskCreated(taskCount, msg.sender, _description, _budget, _endDate);
     }
 
-    function assignExecutor(uint _taskId, address _executor) external {
+    function assignExecutor(uint _taskId, address _executor) external existingTask(_taskId) {
         Task storage task = tasks[_taskId];
 
         require(task.owner == msg.sender, "TaskManager.assignExecutor: Only owner can perform this operation!");
@@ -59,18 +67,18 @@ contract TaskManager {
         emit TaskAssigned(_taskId, _executor);
     }
 
-    function completeTask(uint _taskId) external {
+    function completeTask(uint _taskId) external existingTask(_taskId) {
         Task storage task = tasks[_taskId];
         require(task.executor == msg.sender, "TaskManager.completeTask: Only executor can complete the task!");
         require(!task.isCompleted, "TaskManager.completeTask: Task already completed!");
         require(block.timestamp <= task.endDate, "TaskManager.completeTask: Deadline has passed!");
 
-        task.isCompleted= true;
+        task.isCompleted = true;
 
         emit TaskCompleted(_taskId);
     }
 
-    function getTask(uint _taskId) public view returns (address owner, string memory description,
+    function getTask(uint _taskId) public view existingTask(_taskId) returns (address owner, string memory description,
         uint256 budget, uint256 endDate, address executor, bool isCompleted) {
 
         Task memory task = tasks[_taskId];
@@ -81,7 +89,8 @@ contract TaskManager {
         task.budget,
         task.endDate,
         task.executor,
-        task.isCompleted
+        task.isCompleted,
+        task.isConfirmed
         );
     }
 
@@ -93,5 +102,41 @@ contract TaskManager {
         }
 
         return taskArray;
+    }
+
+
+    function taskExists(uint taskId) internal view returns (bool) {
+        return taskId > 0 && taskId <= taskCount;
+    }
+
+    function acceptTask(uint taskId) external payable existingTask(_taskId) {
+        Task storage task = tasks[taskId];
+
+        require(task.executor == address(0), "TaskManager.acceptTask: Task already accepted");
+        require(msg.sender != task.owner, "TaskManager.acceptTask: Task owner cannot accept their own task");
+
+        // Призначення виконавця
+        task.executor = msg.sender;
+
+        emit TaskAssigned(taskId, msg.sender);
+    }
+
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function confirmTaskCompletion(uint _taskId) external {
+        Task memory task = tasks[_taskId];
+
+        require(taskExists(_taskId), "TaskManager.confirmTaskCompletion: Task doesn't exists");
+        require(msg.sender == task.owner, "TaskManager.confirmTaskCompletion: Task does not exist");
+        require(task.isCompleted, "TaskManager.confirmTaskCompletion: Task is not completed");
+
+        task.isConfirmed = true;
+
+        // Переводимо кошти виконавцю
+        payable(task.executor).transfer(task.budget);
+
+        emit TaskConfirmed(_taskId, msg.sender);
     }
 }

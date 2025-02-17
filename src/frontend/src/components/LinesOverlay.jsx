99ffb29c745f4404974c58468586c6cb81c7b441
VV0lfr4am <vladlen.tsykin@gmail.com>
2025-02-17 20:18:54 +0200

    lines finished

--- src/frontend/src/components/LinesOverlay.jsx
+++ src/frontend/src/components/LinesOverlay.jsx
@@ -3,11 +3,21 @@ import '../styles/LinesOverlay.css';
 const LinesOverlay = () => {
     return (
         <div className="lines-overlay">
-            <div className="line line-small-1"></div>
-            <div className="line line-middle-1"></div>
-            <div className="line line-long"></div>
-            <div className="line line-middle-2"></div>
-            <div className="line line-small-2"></div>
+            <div className="g-1">
+                <div className="line line-small-1"></div>
+                <div className="line line-middle-1"></div>
+                <div className="line line-long"></div>
+                <div className="line line-middle-2"></div>
+                <div className="line line-small-2"></div>
+            </div>
+            <div className="line line-separator"></div>
+            <div className="g-2">
+                <div className="line line-small-1"></div>
+                <div className="line line-middle-1"></div>
+                <div className="line line-long"></div>
+                <div className="line line-middle-2"></div>
+                <div className="line line-small-2"></div>
+            </div>
         </div>
     );
 };

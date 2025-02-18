9e9e6eec4ae89444daf809432c4a2440650a41da
VV0lfr4am <vladlen.tsykin@gmail.com>
2025-02-18 17:00:18 +0200

    add connect wallet functions

--- src/frontend/src/components/Button.jsx
+++ src/frontend/src/components/Button.jsx
@@ -2,13 +2,8 @@ import styles from '../styles/Button.module.css';
 import {useCallback, useState} from 'react';
 
 
-function Button({text, className}) {
-    const handleClick = useCallback(() => {
-        setCount((prevCount) => prevCount + 1);
-        alert('You clicked me!');
-    }, []) ;
-
-    return <button className={className} onClick={handleClick}>{text}</button>;
+function Button({text, className, onClick}) {
+    return <button className={className} onClick={onClick}>{text}</button>;
 }
 
 export default Button;
--- src/frontend/src/components/Navbar.jsx
+++ src/frontend/src/components/Navbar.jsx
@@ -2,9 +2,54 @@ import NavItem from "./NavItem";
 import Button from "./Button";
 import navStyles from '../styles/Navbar.module.css';
 import logo from "../assets/logoMini.svg";
+import {useCallback, useEffect} from "react";
+import {useState} from "react";
 
 
 function Navbar() {
+    const [walletAddress, setWalletAddress] = useState("");
+    useEffect(() => {
+        getCurrentWalletConnected().catch((err) => console.error("Error fetching wallet:", err));
+        addAccountChangeListener().catch((err) => console.error("Error updating wallet:", err));
+    }, []);
+
+    const connectWallet = useCallback(async () => {
+        if (!window.ethereum) {
+            alert("Metamask is not installed");
+            return;
+        }
+        if (walletAddress.length > 0) {
+            return;
+        }
+        const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+        setWalletAddress(accounts[0]);
+
+    }, []);
+
+    const getCurrentWalletConnected = useCallback(async () => {
+        if (!window.ethereum) {
+            alert("Metamask is not installed");
+            return;
+        }
+        try {
+            const accounts = await window.ethereum.request({method: 'eth_accounts'});
+            if (accounts && accounts.length > 0) {
+                setWalletAddress(accounts[0]);
+            } else {
+                console.log("Connect to Metamask using the connect button");
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }, []);
+
+    const addAccountChangeListener = async () => {
+        window.ethereum.on("accountsChanged", (accounts) => {
+            setWalletAddress(accounts[0]);
+        });
+    }
+
+
     return (
         <nav className={navStyles.navbar}>
             <img src={logo} alt="logo" className="logo-img"/>
@@ -15,7 +60,9 @@ function Navbar() {
                 <NavItem className={navStyles.navItem} text="How it works" href="#how-it-works"/>
                 <NavItem className={navStyles.navItem} text="Contacts" href="#contact"/>
             </div>
-            <Button text="Connect Wallet" className={navStyles.btn}/>
+            <Button onClick={connectWallet}
+                    text={walletAddress && walletAddress.length > 0 ? `${walletAddress.substring(0, 6)}...${walletAddress.substring(38)}` : "Connect Wallet"}
+                    className={navStyles.btn}/>
         </nav>
     );
 }

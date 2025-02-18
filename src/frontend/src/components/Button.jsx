97b87c78509a07893c3c931294f958335ee953a9
VV0lfr4am <vladlen.tsykin@gmail.com>
2025-02-18 21:51:51 +0200

    add loading submit logic

--- src/frontend/src/components/Button.jsx
+++ src/frontend/src/components/Button.jsx
@@ -2,8 +2,8 @@ import styles from '../styles/Button.module.css';
 import {useCallback, useState} from 'react';
 
 
-function Button({text, className, onClick}) {
-    return <button className={className} onClick={onClick}>{text}</button>;
+function Button({text, className, onClick, disabled}) {
+    return <button className={className} onClick={onClick} disabled={disabled}>{text}</button>;
 }
 
 export default Button;
--- src/frontend/src/components/TaskForm.jsx
+++ src/frontend/src/components/TaskForm.jsx
@@ -6,7 +6,7 @@ import "../styles/TaskForm.css";
 
 
 function TaskForm({addTask}) {
-
+    const [loading, setLoading] = useState(false);
     const [task, setTask] = useState({description : "", budget: "", endDate: ""})
 
     const handleChange = (e) => {
@@ -20,20 +20,28 @@ function TaskForm({addTask}) {
         }
         const contract = await getContract();
         const date = Math.floor(new Date(task.endDate).getTime() / 1000);
-        const transaction = await contract.createTask(task.description, task.budget, date, {value: ethers.parseEther(task.budget)});
-        await transaction.wait();
-        console.log(transaction);
-        await contract.once("TaskCreated", (taskId, owner, description, budget, endDate) => {
-            console.log(`✅ Event detected: TaskCreated`);
-            console.log(`Task ID: ${taskId}`);
-            console.log(`Description: ${description}`);
-            console.log(`Budget: ${ethers.formatEther(budget)} ETH`);
-            console.log(`End Date: ${new Date(endDate * 1000).toLocaleString()}`);
-            console.log(`Owner: ${owner}`);
-        });
-        //addTask(task);
-        setTask({description : "", budget: "", endDate: ""});
-        alert("Task is created!");
+        try {
+            setLoading(true);
+            const transaction = await contract.createTask(task.description, task.budget, date, {value: ethers.parseEther(task.budget)});
+            await transaction.wait();
+            console.log(transaction);
+            await contract.once("TaskCreated", (taskId, owner, description, budget, endDate) => {
+                console.log(`✅ Event detected: TaskCreated`);
+                console.log(`Task ID: ${taskId}`);
+                console.log(`Description: ${description}`);
+                console.log(`Budget: ${ethers.formatEther(budget)} ETH`);
+                console.log(`End Date: ${new Date(endDate * 1000).toLocaleString()}`);
+                console.log(`Owner: ${owner}`);
+            });
+            //addTask(task);
+            setTask({description : "", budget: "", endDate: ""});
+            alert("Task is created!");
+        }catch (err) {
+            console.error("Transaction failed:", error);
+            alert("Transaction failed ❌");
+        }finally {
+            setLoading(false);
+        }
     };
 
     return (
@@ -44,7 +52,7 @@ function TaskForm({addTask}) {
                 <input type="number" name = "budget" placeholder="Budget (ETH)" value={task.budget} onChange={handleChange} required/>
                 <label htmlFor ="endDate"></label>
                 <input type="text" name = "endDate" placeholder="Deadline" value={task.endDate} onChange={handleChange} required/>
-                <Button text="Publish the task"/>
+                <Button text={loading ? "Publishing..." : "Publish the task"} disabled={loading} className={`submit-button ${loading ? "disabled" : ""}`}/>
             </form>
     );
 }
